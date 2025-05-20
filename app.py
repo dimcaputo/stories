f6165d1909fec15802f26d52de8e91c4dc9b0fa1
dimcaputo <dim.caputo@gmail.com>
2025-05-20 15:02:58 +0200

    Ollama part commented out
     there is a problem with it that prevents from displaying the image

--- app.py
+++ app.py
@@ -15,32 +15,27 @@ story_path = 'static/stories'
 def upload_image():
     if request.method == 'POST':
         file = request.files['img']
-        h = Image.open(file.stream).height
-        w = Image.open(file.stream).height
         filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD'], filename))
         img = os.path.join(app.config['UPLOAD'], filename)
-
-        with open(os.path.join(img_path, filename), 'rb') as f:
-            response = ollama.chat(
-                model='gemma3:4b',
-                messages=[
-                    {
-                    'role': 'user',
-                    'content': 'Tell me a short story about this image. Make it 10 sentences.',
-                    'images': [f.read()],
-                    },
-                ],
-            )
-            story = response['message']['content']
-        with open(os.path.join(story_path, filename), 'w') as s:
-            s.write(story)
-        return render_template('index.html', img=img, story=story)
+        file.save(img)
+        
+        # with open(img, 'rb') as f:
+        #     response = ollama.chat(
+        #         model='tinyllama',
+        #         messages=[
+        #             {
+        #             'role': 'user',
+        #             'content': 'Tell me a short story about this image. Make it 10 sentences.',
+        #             'images': [f.read()],
+        #             },
+        #         ],
+        #     )
+        #     story = response['message']['content']
+        # with open(os.path.join(story_path, filename), 'w') as s:
+        #     s.write(story)
+        return render_template('index.html', img=img)
     return render_template('index.html')
 
-@app.route("/gallery.html", methods=['GET'])
-def display_image():
-    return render_template('gallery.html', image='static/uploads/donnie-rosie-zC9U86g73xg-unsplash.jpg')
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port=5000)
