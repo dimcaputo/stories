dd97ea2a95ae8b7a8d5a54aec07b6a29bc2e2c82
dimcaputo <dimitri.caputo@ygreky.com>
2025-05-21 15:28:03 +0200

    Modified so that the story making is in the back ground, but doesnt quite work yet. Previous commit enough for the need of the project but not pretty

--- app.py
+++ app.py
@@ -1,40 +1,60 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
 import os
 import ollama
 from PIL import Image
+from flask_executor import Executor
 
 app = Flask(__name__)
 
-app.config['UPLOAD'] = 'static/uploads'
+executor = Executor(app)
 
+app.config['UPLOAD'] ='static/uploads'
+stories_path = 'static/stories'
 img_path = 'static/uploads'
-story_path = 'static/stories'
 
 @app.route("/", methods=['GET','POST'])
 def upload_image():
+    if request.method == 'GET':
+        return render_template('index.html')
     if request.method == 'POST':
         file = request.files['img']
         filename = secure_filename(file.filename)
-        img = os.path.join(app.config['UPLOAD'], filename)
+        img = os.path.join(img_path, filename)
         file.save(img)
-        
-        with open(img, 'rb') as f:
-            response = ollama.chat(
-                model='gemma3:4b',
-                messages=[
-                    {
-                    'role': 'user',
-                    'content': "Tell me a short story about this image in 10 sentences.",
-                    'images': [f.read()],
-                    },
-                ],
-            )
-            story = response['message']['content']
-        with open(os.path.join(story_path, filename.rstrip('jpg')+'txt'), 'w') as s:
-            s.write(story)
-        return render_template('index.html', img=img, story=story)
-    return render_template('index.html')
+        url = url_for('startBackgroundStory', img=img, filename=filename)
+        return redirect(url)
+
+@app.route('/startBackgroundStory', methods=['GET', 'POST'])
+def startBackgroundStory():
+    img = request.args.get('img')
+    filename = request.args.get('filename')
+    executor.submit(backgroundStory, img, filename)
+    return redirect(url_for('display_story', img=img, filename=filename))
+
+@app.route('/writer', methods=['GET', 'POST'])
+def backgroundStory(img, filename):
+    with open(img, 'rb') as f:
+        response = ollama.generate(
+            model='qwen2.5vl:3b',
+            prompt= "Write a short story about this image. Make it 10 sentences long",
+            images= [f.read()],
+            options={'num_thread':16}
+        )
+        story = response['response']
+        story_path = filename.rstrip('jpg')+'txt'
+    with open(os.path.join(stories_path, story_path), 'w') as s:
+        s.write(story)
+    return redirect(url_for('display_story', img=img, story=story))
+
+@app.route('/display_story', methods=['GET', 'POST'])
+def display_story():
+    img = request.args.get('img')
+    if not os.path.exists(img.rstrip('jpg')+'txt'):
+        return redirect(url_for('display_story', img=img))
+    story = request.args.get('story')
+    return render_template('/story.html', img=img, story=story)
+    
 
 
 if __name__ == '__main__':
